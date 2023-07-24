504d75d4050fca1cbd4e52f886725c9a2014c3c0
duy-vuong160102 <duyvuong1601@gmail.com>
2023-07-24 21:04:44 +0700

    Initial commit

---
+++ CGO_Min/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Min
+{
+    internal class Program
+    {
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            int index = MinValue(arr);
+            Console.WriteLine("Gia tri nho nhat trong mang la: " + arr[index]);
+            Console.ReadKey();
+    }
+    }
+}
